c58aea69658593691b6216abe674482c25c1091b
aliqa <aliqa@LAPTOP-S2GMTHOI.mynetworksettings.com>
2023-07-30 09:56:12 -0400

    Hello

--- src/homework/DemoHomeWork.java
+++ src/homework/DemoHomeWork.java
@@ -4,6 +4,10 @@ public class DemoHomeWork {
 public static void main(String[] args) {
 	System.out.println("Home work");
 	System.out.println("Done HomeWork");
-		
 }
+		public static void getValue(int a,int b) {
+			System.out.println(a+b);
+		}
+		
+
 }
